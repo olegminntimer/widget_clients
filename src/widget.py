15a385ac5e9adfe58abc7be8f77acf2a83a44f82
Олег Т. <olegtimer@yandex.ru>
2024-08-24 14:57:24 +0500

    Finish2

--- src/widget.py
+++ src/widget.py
@@ -1,33 +1,35 @@
-import masks
+from src import masks
 
 input_data = [
-    'Maestro 1596837868705199',
-    'Счет 64686473678894779589',
-    'MasterCard 7158300734726758',
-    'Счет 35383033474447895560',
-    'Visa Classic 6831982476737658',
-    'Visa Platinum 8990922113665229',
-    'Visa Gold 5999414228426353',
-    'Счет 73654108430135874305',
-    'Неправильно ввели номер карты!',
+    "Maestro 1596837868705199",
+    "Счет 64686473678894779589",
+    "MasterCard 7158300734726758",
+    "Счет 35383033474447895560",
+    "Visa Classic 6831982476737658",
+    "Visa Platinum 8990922113665229",
+    "Visa Gold 5999414228426353",
+    "Счет 73654108430135874305",
+    "Неправильно ввели номер карты!",
 ]
 
 
-def mask_account_card(account: str)->str:
-    ''' Функция возвращает замаскированный номер '''
+def mask_account_card(account: str) -> str:
+    """Функция возвращает замаскированный номер"""
 
-    account_list = account.split() # Создаем список слов
+    account_list = account.split()  # Создаем список слов
 
-    if account_list[0] == 'Неправильно': # Выходим если введена ошибочная запись
-        return account
+    account_name = " ".join(account_list[:-1])  # Название без номера
 
-    account_name = ' '.join(account_list[:-1]) # Название без номер
+    if account_list[0] == "Неправильно":  # Выходим если введена ошибочная запись
+        new_account = account
+    #  Для карты
+    elif len(account_list[-1]) == 16:
+        new_account = account_name + " " + masks.get_mask_card_number(account_list[-1])
+    # Для счёта
+    elif len(account_list[-1]) == 20:
+        new_account = account_name + " " + masks.get_mask_account(account_list[-1])
+    return new_account
 
-    if len(account_list[-1]) == 16: #  Для карты
-        return account_name + ' ' + masks.get_mask_card_number(account_list[-1])
 
-    if len(account_list[-1]) == 20: # Для счёта
-        return account_name + ' ' + masks.get_mask_account(account_list[-1])
-
-for i in input_data: # Тест
+for i in input_data:  # Тест
     print(mask_account_card(i))

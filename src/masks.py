20db63150bb8df5e5cddcda2d5fab8313d8ec530
Олег Т. <olegtimer@yandex.ru>
2024-08-24 13:48:37 +0500

    Progress in the past

---
+++ src/masks.py
@@ -0,0 +1,32 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[str, int]) -> str:
+    """Функция get_mask_card_number принимает на вход номер карты и возвращает ее маску."""
+
+    card_number = str(card_number)  # Введенное приводим к типу str
+
+    if card_number.isdigit() and len(card_number) == 16:
+        return card_number[0:4] + " " + card_number[4:6] + "** ****" + card_number[12:]
+    else:
+        return "Неправильно ввели номер карты!"
+
+
+def get_mask_account(account: Union[str, int]) -> str:
+    """Функция get_mask_account принимает на вход номер счёта и возвращает его маску."""
+
+    account = str(account)  # Введенное приводим к типу str
+
+    if account.isdigit() and len(account) == 20:
+        return "**" + account[16:]
+    else:
+        return "Неправильно ввели номер счёта!"
+
+
+# card_number = input('Введите номер карты: ')
+# print(get_mask_card_number(card_number))
+# print(get_mask_card_number(1234567812345679))
+
+
+# account = input('Введите номер счёта: ')
+# print(get_mask_account(account))

a162c37a8978e4a5049eecd6aa61cc4f5b44da6b
Олег Т. <olegtimer@yandex.ru>
2024-08-29 10:20:23 +0500

    add processing.py

---
+++ src/processing.py
@@ -0,0 +1,14 @@
+from typing import List, Dict, Any
+
+
+def filter_by_state(dict_list:List[Dict],state:str='EXECUTED')->List[Dict[Any,Any]]:
+    ''' Функция возвращает список словарей по ключу state '''
+
+    new_dict_list = []
+    return new_dict_list
+
+def sort_by_date(dict_list:List[Dict],sort_by:bool=True)->List[Dict[Any,Any]]:
+    ''' Функция сортирует список словарей по дате (по умолчанию - убывание) '''
+
+    new_dict_list = []
+    return new_dict_list

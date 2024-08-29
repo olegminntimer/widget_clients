0260ae2e8dcf0fc12eb83bc4814d1fbfb78d36f7
Олег Т. <olegtimer@yandex.ru>
2024-08-29 12:13:51 +0500

    I checked the files using black, isort, flake8, mypy

--- main.py
+++ main.py
@@ -15,25 +15,27 @@ input_data = [
 
 date_old = "2024-03-11T02:26:18.671407"
 
-DL = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+DL = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 
 
 def main():
-    ''' Основная вызывающая функция '''
-    # print(get_date(date_old))
-    #
-    # for i in input_data:  # Тест
-    #     print(mask_account_card(i))
+    """Основная вызывающая функция"""
+    print(get_date(date_old))
+
+    for i in input_data:  # Тест
+        print(mask_account_card(i))
 
     list_of_dict = filter_by_state(DL)
     for i in list_of_dict:
         print(i)
     print()
 
-    list_of_dict = filter_by_state(DL,'CANCELED')
+    list_of_dict = filter_by_state(DL, "CANCELED")
     for i in list_of_dict:
         print(i)
     print()
@@ -49,5 +51,5 @@ def main():
     print()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

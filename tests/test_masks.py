81782501849d4cfc8470a4c942d92d8786e37cc3
Олег Т. <olegtimer@yandex.ru>
2024-08-30 11:46:24 +0500

    changed test_masks/py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,18 +1,21 @@
+import pytest
+
 from src.masks import get_mask_card_number, get_mask_account
 
+@pytest.mark.parametrize("x, expected", [("1596837868705199", "1596 83** **** 5199"),
+                                         (1596837868705199, "1596 83** **** 5199"),
+                                         ("159683786870519a", "Неправильно ввели номер карты!"),
+                                         (15968, "Неправильно ввели номер карты!"),
+                                         ("15968", "Неправильно ввели номер карты!"),
+                                         ('',"Неправильно ввели номер карты!")])
+def test_get_mask_card_number(x, expected):
+    assert get_mask_card_number(x) == expected
 
-def test_get_mask_card_number():
-    assert get_mask_card_number("1596837868705199") == "1596 83** **** 5199"
-    assert get_mask_card_number(1596837868705199) == "1596 83** **** 5199"
-    assert get_mask_card_number("159683786870519a") == "Неправильно ввели номер карты!"
-    assert get_mask_card_number(15968) == "Неправильно ввели номер карты!"
-    assert get_mask_card_number("15968") == "Неправильно ввели номер карты!"
-    assert get_mask_card_number('') == "Неправильно ввели номер карты!"
-
-def test_get_mask_account():
-    assert get_mask_account("64686473678894779589") == "**9589"
-    assert get_mask_account(64686473678894779589) == "**9589"
-    assert get_mask_account("ad686473678894779589") == "Неправильно ввели номер счёта!"
-    assert get_mask_account("646864") == "Неправильно ввели номер счёта!"
-    assert get_mask_account(646864) == "Неправильно ввели номер счёта!"
-    assert get_mask_account("") == "Неправильно ввели номер счёта!"
+@pytest.mark.parametrize("x, expected", [("64686473678894779589", "**9589"),
+                                         (64686473678894779589, "**9589"),
+                                         ("ad686473678894779589", "Неправильно ввели номер счёта!"),
+                                         ("646864", "Неправильно ввели номер счёта!"),
+                                         (646864, "Неправильно ввели номер счёта!"),
+                                         ("", "Неправильно ввели номер счёта!")])
+def test_get_mask_account(x, expected):
+    assert get_mask_account(x) == expected

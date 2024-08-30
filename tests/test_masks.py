600c365989853af290f3f531a14e79076e3a1960
Олег Т. <olegtimer@yandex.ru>
2024-08-30 11:23:56 +0500

    add test_get_mask_card_number test_get_mask_account

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,8 +1,18 @@
-from src.masks import get_mask_card_number
+from src.masks import get_mask_card_number, get_mask_account
 
 
 def test_get_mask_card_number():
+    assert get_mask_card_number("1596837868705199") == "1596 83** **** 5199"
     assert get_mask_card_number(1596837868705199) == "1596 83** **** 5199"
-    assert get_mask_card_number('1596837868705199') == "1596 83** **** 5199"
-    assert get_mask_card_number() == ""
-    assert get_mask_card_number('') == ""
+    assert get_mask_card_number("159683786870519a") == "Неправильно ввели номер карты!"
+    assert get_mask_card_number(15968) == "Неправильно ввели номер карты!"
+    assert get_mask_card_number("15968") == "Неправильно ввели номер карты!"
+    assert get_mask_card_number('') == "Неправильно ввели номер карты!"
+
+def test_get_mask_account():
+    assert get_mask_account("64686473678894779589") == "**9589"
+    assert get_mask_account(64686473678894779589) == "**9589"
+    assert get_mask_account("ad686473678894779589") == "Неправильно ввели номер счёта!"
+    assert get_mask_account("646864") == "Неправильно ввели номер счёта!"
+    assert get_mask_account(646864) == "Неправильно ввели номер счёта!"
+    assert get_mask_account("") == "Неправильно ввели номер счёта!"

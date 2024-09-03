87e3cf19b8f2ca17271890be1212603458a1a19c
Олег Т. <olegtimer@yandex.ru>
2024-09-03 17:52:18 +0500

    add card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -1,3 +1,6 @@
+from curses.ascii import isdigit
+
+
 def filter_by_currency(dict_list: list, currency:str):
     """ Функция-генератор, которая выдает транзакции из входного списка
      транзакций dict_list в соответствии с заданной валютой currency """
@@ -5,3 +8,15 @@ def filter_by_currency(dict_list: list, currency:str):
     for i in dict_list:
         if i.get("operationAmount").get("currency").get("code") == currency:
             yield i
+
+
+def card_number_generator(start:int, finish:int):
+    """ Функция-генератор, которая генерирует номера карт
+     в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999 """
+
+    # if (isdigit(start) and isdigit(finish) and
+    # (0 <= start <= 9999999999999999) and (0 <= finish <= 9999999999999999)):
+    max_width = 16
+    for i in range(start, finish):
+        value_str = f"{i:0{max_width}}"
+        yield f"{value_str[:4]} {value_str[4:8]} {value_str[8:12]} {value_str[12:16]}"

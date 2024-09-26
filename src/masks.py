44dcb5005bcafafaff71595fcb84bbf7d31dd5e9
Oleg Timershayahov <olegtimer@yandex.ru>
2024-09-26 21:58:43 +0500

    add Adjustment

--- src/masks.py
+++ src/masks.py
@@ -4,25 +4,26 @@ from pathlib import Path
 from typing import Union
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-file_name = str(BASE_DIR / 'logs' / 'masks.log')
+file_name = str(BASE_DIR / "logs" / "masks.log")
 
 logger = logging.getLogger(__name__)
 file_handler = logging.FileHandler(file_name)
-file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 logger.setLevel(DEBUG)
+
 
 def get_mask_card_number(card_number: Union[str, int]) -> str:
     """Функция get_mask_card_number принимает на вход номер карты и возвращает ее маску."""
 
     card_number = str(card_number)  # Введенное приводим к типу str
-    logger.debug('Validating the card')
+    logger.debug("Validating the card")
     if card_number.isdigit() and len(card_number) == 16:
-        logger.debug('Successful verification')
+        logger.debug("Successful verification")
         return card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[12:]
     else:
-        logger.error('Incorrect format')
+        logger.error("Incorrect format")
         return "Неправильно ввели номер карты!"
 
 
@@ -30,10 +31,10 @@ def get_mask_account(account: Union[str, int]) -> str:
     """Функция get_mask_account принимает на вход номер счёта и возвращает его маску."""
 
     account = str(account)  # Введенное приводим к типу str
-    logger.debug('Validating the account')
+    logger.debug("Validating the account")
     if account.isdigit() and len(account) == 20:
-        logger.debug('Successful verification')
+        logger.debug("Successful verification")
         return "**" + account[16:]
     else:
-        logger.error('Incorrect format')
+        logger.error("Incorrect format")
         return "Неправильно ввели номер счёта!"
--- src/utils.py
+++ src/utils.py
@@ -5,31 +5,31 @@ from logging import DEBUG
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-file_name = str(BASE_DIR / 'logs' / 'utils.log')
+file_name = str(BASE_DIR / "logs" / "utils.log")
 
 logger = logging.getLogger(__name__)
 file_handler = logging.FileHandler(file_name)
-file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 logger.setLevel(DEBUG)
+
 
 def get_json(path_to_file: str) -> list:
     """Функция принимает путь до файла и возвращает данные в формате json"""
 
     try:
-        logger.debug('Opening the file...')
+        logger.debug("Opening the file...")
         with open(path_to_file, encoding="utf-8") as f:
             data = json.load(f)
-            logger.debug('The data was considered')
+            logger.debug("The data was considered")
             return data
     except JSONDecodeError:
-        logger.error('JSONDecodeError - Invalid JSON data.')
+        logger.error("JSONDecodeError - Invalid JSON data.")
         return []
     except FileNotFoundError:
-        logger.error('FileNotFoundError - The file does not exist.')
+        logger.error("FileNotFoundError - The file does not exist.")
         return []
     except ValueError:
-        logger.error('ValueError - Invalid JSON data.')
+        logger.error("ValueError - Invalid JSON data.")
         return []
-
